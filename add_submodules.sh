d6131262e38dc21567bb40f9dbff1d372a9af8e5
Juri Tomak <juritomak@Mac.fritz.box>
2025-02-22 16:32:33 +0100

    feat: added submodules from RAST

---
+++ add_submodules.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git submodule add https://github.com/jtpgames/Locust_Scripts locust_scripts
+git submodule add https://github.com/jtpgames/Simulators Simulators
+git submodule add https://github.com/jtpgames/RAST-Common-Python.git RAST-Common-Python
