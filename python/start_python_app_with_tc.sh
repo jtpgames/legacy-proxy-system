66b14393f8f1039cccc31fd938c828ebd66e5245
Juri Tomak <juritomak@Mac.fritz.box>
2025-07-17 21:01:35 +0200

    feat(python): WIP use linux tc to simulate an ADSL connection for ingress and egress traffic.

--- python/start_python_app_with_tc.sh
+++ python/start_python_app_with_tc.sh
@@ -11,6 +11,41 @@ if [ ! -f "$1" ]; then
   exit 1
 fi
 
+apt-get update && apt-get install -y iproute2 netcat-openbsd bc
+tc -V
+
+# Defaults
+download_rate_mbit="2"  # in mbit
+latency_ms=100          # e.g., 100 for 100ms
+upload_bandwidth="1"    # in mbit
+egress_delay="40ms"
+egress_jitter="10ms"
+
+# Calculate burst = (rate in bits per second * latency in seconds) / 8
+rate_bit=$((download_rate_mbit * 1000 * 1000))
+latency_sec=$(echo "$latency_ms / 1000" | bc -l)
+
+echo "$rate_bit * $latency_sec / 8"
+burst_bytes=$(echo "$rate_bit * $latency_sec / 8" | bc -l)
+
+# Round to nearest integer
+burst_rounded=$(printf "%.0f" "$burst_bytes")
+echo $burst_rounded
+
+download_burst=$(($burst_rounded / 1000))
+echo $download_burst
+
+# Calculate burst = (rate in bits per second * latency in seconds) / 8
+rate_bit=$((upload_bandwidth * 1000 * 1000))
+latency_sec=$(echo "$latency_ms / 1000" | bc -l)
+
+burst_bytes=$(echo "$rate_bit * $latency_sec / 8" | bc -l)
+
+# Round to nearest integer
+burst_rounded=$(printf "%.0f" "$burst_bytes")
+
+upload_burst=$(($burst_rounded / 1000))
+
 apt-get update && apt-get install -y iproute2 netcat-openbsd 
 tc -V
 
@@ -19,16 +54,40 @@ echo "Before: $(ulimit -n)"
 ulimit -n 65536 || echo "Failed to increase ulimit"
 echo "After: $(ulimit -n)"
 
-# Install Python dependencies and run script
-pip install -r requirements.txt
+# Install Python dependencies
+pip install --root-user-action=ignore -r requirements.txt
 
 # Limit all incoming and outgoing network to 1mbit/s
-#
-# # Set up ingress policing (incoming traffic limit)
+# Simulate ADSL link
 tc qdisc add dev eth0 ingress
-tc filter add dev eth0 parent ffff: protocol ip prio 50 u32 match ip src 0.0.0.0/0 police rate 1mbit burst 10k drop flowid :1
 
-# Set up TBF for egress shaping (outgoing traffic limit)
-tc qdisc add dev eth0 root tbf rate 1mbit latency 25ms burst 10k
+echo "Set up ingress policing (incoming traffic limit) with rate ${download_rate_mbit}mbit burst ${download_burst}k"
+tc filter add dev eth0 parent ffff: protocol ip prio 50 u32 match ip src 0.0.0.0/0 police rate "${download_rate_mbit}mbit" burst "${download_burst}k" drop flowid :1
+
+echo "Apply TBF for uplink/egress shaping (limited upload/outgoing rate) with rate ${upload_bandwidth}mbit burst ${upload_burst}k latency ${latency_ms}ms"
+tc qdisc add dev eth0 root handle 1: tbf rate "${upload_bandwidth}mbit" burst "${upload_burst}k" latency "${latency_ms}ms"
+
+# echo "Add netem to simulate latency and jitter with delay $egress_delay $egress_jitter distribution normal"
+# tc qdisc add dev eth0 parent 1:1 handle 10: netem delay $egress_delay $egress_jitter distribution normal
+
+# Try to use tc to simulate these links that we simulated in other experiments using mininet.
+# # Simulate production system
+# # customer has 1 Gigabit Ethernet (GbE) connection to his router
+# linkopts = {'bw': 1000, 'delay': '0.45ms'}
+# self.addLink(locust_runner, customerSwitch, **linkopts)
+#
+# # customer has VDSL 100 (100 MBit Download, 30 MBit Upload)
+# linkopts = {'bw': 30, 'delay': '2.4ms', 'jitter': '5.2ms'}
+# self.addLink(customerSwitch, ispCustomerSwitch, **linkopts)
+#
+# # ISP Interlink (10 GbE connection)
+# # 1 Gigabit is the maximum bandwidth allowed by mininet
+# linkopts = {'bw': 1000, 'delay': '13.3ms', 'jitter': '3.15ms'}
+# self.addLink(ispCustomerSwitch, ispAPSwitch, **linkopts)
+#
+# # alarm provider sadly does NOT have SDSL, rather LACP-based Uplinks: VDSL100, VDSL50 combined
+# linkopts = {'bw': 50, 'delay': '7.97ms', 'jitter': '2.9ms'}
+# self.addLink(ispAPSwitch, self.apSwitch, **linkopts)
+
 
 python "$1"
