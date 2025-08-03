cad57aa43e40f34e4c7212fe36df12a9cb598337
Juri Tomak <juritomak@Mac.fritz.box>
2025-08-03 16:31:01 +0200

    feat(python): use horizontal layout when visualizing the docker-compose infrastructures

--- python/visualize-docker-compose-system.sh
+++ python/visualize-docker-compose-system.sh
@@ -4,10 +4,10 @@ docker run --platform linux/amd64 --rm -it \
   --name dcv \
   -v "$(pwd)":/input \
   pmsipilot/docker-compose-viz \
-  render -m image --force --no-volumes -o docker-compose-legacy.png docker-compose-legacy.yml
+  render -m image --force --no-volumes --horizontal -o docker-compose-legacy.png docker-compose-legacy.yml
 
 docker run --platform linux/amd64 --rm -it \
   --name dcv \
   -v "$(pwd)":/input \
   pmsipilot/docker-compose-viz \
-  render -m image --force --no-volumes -o docker-compose-ng.png docker-compose-ng.yml
+  render -m image --force --no-volumes --horizontal -o docker-compose-ng.png docker-compose-ng.yml
